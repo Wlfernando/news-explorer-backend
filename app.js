955a771a304f7d0d4c1cb892cde99a15f8532672
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-03-11 18:26:06 -0600

    feat: add signup

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cards = require('./route/cards');
+const { createUser } = require('./handler/user');
 
 const app = express();
 const { PORT = 3001 } = process.env;
+
+mongoose.connect('mongodb://127.0.0.1:27017/newsApi');
+
+app.use('/signup', createUser);
+
+app.use('/cards', cards);
 
 app.listen(PORT);
