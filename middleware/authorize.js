47887c9bd14fc7a529aa59724eb69171c44ec860
Fernando Alcázar Malfavón <4maferal@gmail.com>
2026-03-09 14:25:13 -0600

    fix: include brackets in destructuring

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const { key, rejected, isProduction } = require('../lib/const');
 
 module.exports = function authorize(req, res, next) {
-  const { cookies: { "__Host-authentication": auth, authentication: authDev } } = req;
+  const { cookies: { ["__Host-authentication"]: auth, authentication: authDev } } = req;
   const authentication = isProduction ? auth : authDev;
 
   if (!authentication || !authentication.startsWith('Bearer ')) {
